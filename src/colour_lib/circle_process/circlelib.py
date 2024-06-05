7668572766a219257680a0c8a4eaf6abcc06174a
slendersold <boikov_g@mail.ru>
2024-06-05 20:27:50 +0000

    black formated

--- src/colour_lib/circle_process/circlelib.py
+++ src/colour_lib/circle_process/circlelib.py
@@ -2,10 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from colour import XYZ_to_Lab, delta_E
 
+
 class CircleColors:
     def __init__(self, number_of_circles):
         self.number_of_circles = number_of_circles
-        return
+
+    def R(self):
+        pass
 
     def apply_gammaCorr(img, gammas):
         # Transpose to set channel as 1st for iter
@@ -20,7 +23,7 @@ class CircleColors:
         ret = np.transpose(ret, (1, 2, 0))
         return ret
 
-    def calculate_delta_E (self, observe, reference):
+    def calculate_delta_E(self, observe, reference):
         observe = XYZ_to_Lab(observe)
         reference = XYZ_to_Lab(reference)
         deltas = np.zeros((observe.shape[0], 1))
@@ -29,14 +32,16 @@ class CircleColors:
             b = observe[i, :]
             deltas[i] = delta_E(a, b, method="CIE 2000")
         return deltas
-    
+
     def draw_circle_mask(image, coord, radius):
         mask_shape = image.shape
         coords_y, coords_x = np.ogrid[: mask_shape[0], : mask_shape[1]]
         circle_mask = np.zeros(mask_shape[:2], dtype=bool)
         for idx, centroids in coord.iterrows():
             centroid_y, centroid_x = centroids["Y"], centroids["X"]
-            circle_mask = circle_mask | ((coords_y - centroid_y) ** 2 + (coords_x - centroid_x) ** 2 <= radius**2)
+            circle_mask = circle_mask | (
+                (coords_y - centroid_y) ** 2 + (coords_x - centroid_x) ** 2 <= radius**2
+            )
 
         plt.imshow(image)
         plt.imshow(circle_mask, alpha=0.5)
@@ -44,29 +49,39 @@ class CircleColors:
     def create_circle_mask(image, centroids, radius):
         mask_shape = image.shape[:2]
         coords_y, coords_x = np.ogrid[: mask_shape[0], : mask_shape[1]]
-        circle_mask = (coords_y - centroids["Y"])**2 + (coords_x - centroids["X"])**2 <= radius**2
+        circle_mask = (coords_y - centroids["Y"]) ** 2 + (
+            coords_x - centroids["X"]
+        ) ** 2 <= radius**2
         circle_mask = np.expand_dims(circle_mask, axis=-1)  # Add a channel dimension
-        circle_mask = np.tile(circle_mask, (1, 1, image.shape[2]))  # Tile to match image channels
+        circle_mask = np.tile(
+            circle_mask, (1, 1, image.shape[2])
+        )  # Tile to match image channels
         return circle_mask
 
     def calc_rectangle(self, image, coord, radius):
-        #create an array of mean values of one set of circles from A1 to D6
+        # create an array of mean values of one set of circles from A1 to D6
         mean_values = []
         for idx, centroids in coord.iterrows():
             circle = self.create_circle_mask(image, centroids, radius)
             masked_image = image * circle
             mean_in_circle = []
             for i in range(image.shape[2]):
-                channel_values = masked_image[:,:,i][masked_image[:,:,i] != 0]
+                channel_values = masked_image[:, :, i][masked_image[:, :, i] != 0]
                 mean_in_circle.append(channel_values.mean())
             mean_in_circle = np.nan_to_num(mean_in_circle)  # Replace NaN with 0
             mean_values.append(mean_in_circle.tolist())
         return np.array(mean_values)
-    
+
     def calc_rectangles(self, image, coords, rads, zones):
-        #calculates mean value of all mean values from A1 to D6 in one single image
-        mean_values = np.zeros((self.number_of_circles,image.shape[2]))
+        # calculates mean value of all mean values from A1 to D6 in one single image
+        mean_values = np.zeros((self.number_of_circles, image.shape[2]))
         for idx, v in enumerate(zones):
             mean_values = mean_values + self.calc_rectangle(image, coords[v], rads[v])
-        mean_values = mean_values/(idx+1)
+        mean_values = mean_values / (idx + 1)
         return mean_values
+
+    def calc_slide(mnval_CA, mnval_slide):
+        # appends clear area colors value to the beginning of data array
+        # thus formating in standart form: [CA, A1, ..., D6].
+        full_table = np.append(mnval_CA, mnval_slide, axis=0)
+        return full_table
--- src/colour_lib/regressors/LassoRegressor.py
+++ src/colour_lib/regressors/LassoRegressor.py
@@ -1,15 +1,16 @@
 from sklearn import linear_model
 
-class LassoRegressor():
-  def __init__(self, train_data, reference_data, alpha):
-    self.reg = linear_model.Lasso(alpha = alpha)
-    self.reg.fit(train_data, reference_data)
 
-  def predict(self, img):
-    mod_img = img.copy()
+class LassoRegressor:
+    def __init__(self, train_data, reference_data, alpha):
+        self.reg = linear_model.Lasso(alpha=alpha)
+        self.reg.fit(train_data, reference_data)
 
-    for strip in mod_img:
-      strip = self.reg.predict(strip)
+    def predict(self, img):
+        mod_img = img.copy()
 
-    # return np.clip(mod_img, 0, 1)
-    return mod_img
+        for strip in mod_img:
+            strip = self.reg.predict(strip)
+
+        # return np.clip(mod_img, 0, 1)
+        return mod_img
