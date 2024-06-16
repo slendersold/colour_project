2418dcc7a317243a5f07241a9a632c2d96c1efe2
agurianova <anastasiia.gurianov@gmail.com>
2024-06-16 19:22:51 +0000

    1) palette dir with notebook for palette processing and 2) skripts in colour_lib dir for this notebook

---
+++ src/colour_lib/palette_parser/ImageProcessing.py
@@ -0,0 +1,69 @@
+import numpy as np
+import cv2
+
+class ImageProcessing:
+    def __init__(self):
+        pass
+
+    def gray_blur_thresh(self, image):
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        # Apply Gaussian blur to the grayscale image
+        blurred = cv2.GaussianBlur(gray, (9, 9), 0)
+        
+        # Apply binary thresholding to the blurred image
+        _, thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
+
+        return(thresh)
+
+    def sobel(self, image):
+        """
+        Compute the Sobel magnitude of the input image.
+
+        Parameters:
+        image (numpy array): The input image.
+
+        Returns:
+        numpy array: The Sobel magnitude image.
+        """
+        sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
+        sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
+        image_processed = np.sqrt(sobelx ** 2 + sobely ** 2)
+        image_processed = cv2.convertScaleAbs(image_processed)
+        return image_processed
+    
+    def clahe(self, image):
+        """
+        Enhance the contrast of the image using CLAHE.
+
+        Parameters:
+        image (numpy array): The input image.
+
+        Returns:
+        numpy array: The image with enhanced contrast.
+        """
+        lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(lab)
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+        cl = clahe.apply(l)
+        limg = cv2.merge((cl, a, b))
+        image_processed = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+        return image_processed
+    
+    def gray_blur_canny(self, image):
+        """
+        Preprocess the image to convert to grayscale, apply Gaussian blur, and detect edges.
+
+        Parameters:
+        image (numpy array): The input image.
+
+        Returns:
+        numpy array: The edges detected in the image.
+        """
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (9, 9), 0)
+        image_processed = cv2.Canny(blurred, 50, 150)
+        
+        return image_processed   
---
+++ src/colour_lib/palette_parser/ObjectColor.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class ObjectColor:
+    def __init__(self):
+        pass
+
+    def calculate_rgb(self, coordinates, zarr, filter="_1000"):
+
+        def mean_rgb_circ(image, x, y, r, margin = 0.3):
+
+            r = int(r*margin)
+
+            Y, X = np.ogrid[:image.shape[0], :image.shape[1]]
+            
+            dist_from_center = np.sqrt((X - x)**2 + (Y - y)**2)
+            
+            mask = dist_from_center <= r
+            
+            mean_color = []
+            for i in range(image.shape[2]): 
+                channel_mean = image[:, :, i][mask].mean()
+                mean_color.append(channel_mean)
+            
+            return np.array(mean_color)
+        
+        def mean_rgb_rect(image, y0, y1, x0, x1, margin=0.3):
+
+            y0, y1 = sorted([y0, y1])
+            x0, x1 = sorted([x0, x1])
+
+            h = y1 - y0
+            w = x1 - x0
+
+            y0_m, y1_m = int(y0 + h*margin), int(y1 - h*margin)
+            x0_m, x1_m = int(x0 + w * margin), int(x1 - w * margin)
+
+            region = image[y0_m:y1_m, x0_m:x1_m]
+            mean_color = region.mean(axis=(0, 1))
+
+            return np.array(mean_color)
+
+        coordinates_filtered = {key: value for key, value in coordinates.items() if (filter in key) and ("rect" not in key)}
+        coordinates_filtered['rect_CA'] = coordinates['rect_CA']
+
+        obj_rgb = {}
+        for i, (key, values) in enumerate(coordinates_filtered.items()):
+            print(key)
+            if key == 'rect_CA':
+                x0 = values['x0']
+                x1 = values['x1']
+                y0 = values['y0']
+                y1 = values['y1']
+
+                mean_color = mean_rgb_rect(zarr, y0, y1, x0, x1, margin=0.3)
+                obj_rgb[key] = {'r': mean_color[0], 'g': mean_color[1], 'b': mean_color[2]}
+
+            else:
+                x = values['x_centroid']
+                y = values['y_centroid']
+                r = values['radius']
+                
+                mean_color = mean_rgb_circ(zarr, x, y, r, margin=0.3)
+                obj_rgb[key] = {'r': mean_color[0], 'g': mean_color[1], 'b': mean_color[2]}
+        
+        return obj_rgb
+
+    def plot_palette(self, obj_rgb):
+        """
+        Plot a color grid based on the mean RGB values stored in the obj_rgb dictionary.
+
+        Args:
+        obj_rgb (dict): Dictionary containing mean RGB values for each object.
+        """
+        fig, ax = plt.subplots(figsize=(4, 7))
+
+        # Create a grid of colors based on the obj_rgb keys
+        color_grid = np.zeros((7, 4, 3))  # Assuming 7 rows and 4 columns for CA and A-D with 1-6
+
+        # Fill the color grid with the mean colors from the obj_rgb
+        for key, color in obj_rgb.items():
+            if key == 'rect_CA':
+                for i in range(4):
+                    color_grid[0, i, :] = [color['r'], color['g'], color['b']]
+            else:
+                row_label = key[0]  # Extract the row label (A, B, C, ...)
+                col_label = key[1]  # Extract the column label (6, 5, 4, ...)
+                row_idx = 7 - int(col_label)  # Calculate the row index (6 maps to 0, 5 maps to 1, etc.)
+                col_idx = ord(row_label) - ord('A')  # Calculate the column index (A maps to 0, B maps to 1, etc.)
+                color_grid[row_idx, col_idx, :] = [color['r'], color['g'], color['b']]
+
+        # Normalize the color values to [0, 1] for displaying with imshow
+        color_grid /= 255.0
+
+        # Display the grid
+        ax.imshow(color_grid, aspect='auto')
+
+        # Set the tick labels
+        ax.set_xticks(np.arange(4))
+        ax.set_xticklabels(["A", "B", "C", "D"])
+        ax.set_yticks(np.arange(7))
+        ax.set_yticklabels(["CA", "6", "5", "4", "3", "2", "1"])
+
+        plt.show()
+
+
+
